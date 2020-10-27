f4a01f9ad82d31f4ec29538ae325e67344d9873d
Noble Kalish <NobleKalish>
2020-10-27 19:08:08 -0400

    saving snippet works

--- src/main/java/edu/wpi/cs/eutrepe/lambda/HandleCreateSnippet.java
+++ src/main/java/edu/wpi/cs/eutrepe/lambda/HandleCreateSnippet.java
@@ -25,37 +25,6 @@ public class HandleCreateSnippet implements RequestStreamHandler {
 	final String failureMessage = "Failed to save snippet";
 	LambdaLogger logger;
 
-//    @Override
-//    public CreateSnippetResponse handleRequest(SnippetDto req, Context context) {
-//		logger = context.getLogger();
-//		logger.log(req.toString());
-//		CreateSnippetResponse res = new CreateSnippetResponse();
-//
-//    	SnippetDao snippetDao = new SnippetDao();
-//    	try {
-//    		SnippetDto savedSnippet = req;
-//    		Integer id = snippetDao.addSnippet(req);
-//    		if (id != null) {
-//        		savedSnippet.setId(id);
-//        		res.setHttpCode(200);
-//        		res.setMsg(successMessage);
-//        		res.setSnippet(savedSnippet);
-//    		} else {
-//    			res.setHttpCode(500);
-//    			res.setMsg(failureMessage);
-//    			res.setSnippet(null);
-//    		}
-//    		return res;
-//		} catch (Exception e) {
-//			logger.log(e.getMessage());
-//			e.printStackTrace();
-//			res.setHttpCode(500);
-//			res.setMsg(e.getMessage());
-//			res.setSnippet(null);
-//    		return res;
-//		}
-//    }
-
 	@Override
 	public void handleRequest(InputStream input, OutputStream output, Context context) throws IOException {
 		// TODO Auto-generated method stub
