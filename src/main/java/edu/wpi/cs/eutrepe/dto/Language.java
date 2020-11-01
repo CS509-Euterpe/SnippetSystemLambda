c966302ff864b7b7ee096fa24731762bc7df8ca4
Noble Kalish <NobleKalish>
2020-11-01 14:18:57 -0500

    added more langauges

--- src/main/java/edu/wpi/cs/eutrepe/dto/Language.java
+++ src/main/java/edu/wpi/cs/eutrepe/dto/Language.java
@@ -1,9 +1,16 @@
 package edu.wpi.cs.eutrepe.dto;
 
 public enum Language {
-	JAVA,
-	JAVASCRIPT,
-	PYTHON,
-	C,
-	CPP
+	JAVA("JAVA"), JAVASCRIPT("JAVASCRIPT"), PYTHON("PYTHON"), C("C"), CPP("C++"), BASH("BASH"), GO("GO"), PERL("PERL"), TYPESCRIPT("TYPESCRIPT"), HTML("HTML"), NONE("NONE"), CSHARP("C#");
+
+	private String value;
+
+	private Language(String value)
+	   {
+	      this.value = value;
+	   }
+
+	public String toString() {
+		return this.value;
+	}
 }
