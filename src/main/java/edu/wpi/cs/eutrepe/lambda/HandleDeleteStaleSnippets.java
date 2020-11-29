cb083af8f555150e550c815d69451aa779bc401f
Treis <Treis@DESKTOP-N03O56T.lan>
2020-11-28 22:24:02 -0500

    added DeleteStaleSnippet

--- src/main/java/edu/wpi/cs/eutrepe/lambda/HandleDeleteStaleSnippets.java
+++ src/main/java/edu/wpi/cs/eutrepe/lambda/HandleDeleteStaleSnippets.java
@@ -1,22 +1,84 @@
 package edu.wpi.cs.eutrepe.lambda;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.nio.charset.Charset;
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.List;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+
+import edu.wpi.cs.eutrepe.db.CommentDao;
+import edu.wpi.cs.eutrepe.db.SnippetDao;
+import edu.wpi.cs.eutrepe.dto.CommentDto;
+import edu.wpi.cs.eutrepe.dto.SnippetDto;
 
 public class HandleDeleteStaleSnippets implements RequestStreamHandler {
-
+	LambdaLogger logger;
     @Override
     public void handleRequest(InputStream input, OutputStream output, Context context) throws IOException {
 
-        // TODO: Implement your stream handler. See https://docs.aws.amazon.com/lambda/latest/dg/java-handler-io-type-stream.html for more information.
-        // This demo implementation capitalizes the characters from the input stream.
-        int letter = 0;
-        while((letter = input.read()) >= 0) {
-            output.write(Character.toUpperCase(letter));
+    	logger = context.getLogger();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(input, Charset.forName("US-ASCII")));
+  		PrintWriter writer = new PrintWriter(
+  				new BufferedWriter(new OutputStreamWriter(output, Charset.forName("US-ASCII"))));
+  		
+  		
+  		JsonObject event = new GsonBuilder().create().fromJson(reader, JsonObject.class);
+		
+		
+		int days = new Gson().fromJson(event.get("days"), Integer.class);
+		CommentDao commentDao = new CommentDao();
+  		SnippetDao snippetDao = new SnippetDao();
+  		logger.log("Deleting snippets older than "+days+" days");
+
+             try {
+            	 writer.write("deleting snippets"); 
+            	 
+            	writer.write("deleting comments");
+  				ArrayList<SnippetDto> snippets = snippetDao.getAllSnippets();
+  				
+  				SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.SSS");
+  				
+  				for (SnippetDto cur: snippets)
+  				{
+  					Date parsedDate = dateFormat.parse(cur.getTimestamp());
+  					Timestamp timestamp = new java.sql.Timestamp(parsedDate.getTime());
+  					Timestamp comparator = new java.sql.Timestamp(System.currentTimeMillis()-(days*86400000));
+  					if(timestamp.before(comparator))
+  					{
+  						List<CommentDto> stalecomments = cur.getComments();
+  						for(CommentDto curr: stalecomments)
+  						{
+  							commentDao.deleteComment(curr.getId());
+  						}
+  						snippetDao.deleteSnippet(cur.getId());
+	
+  					}
+  				}
+  				
+  			} catch (Exception e) {
+  				logger.log(e.getMessage());
+  				e.printStackTrace();
+  				writer.write("Failed to get all snippets");
+  			} finally {
+  				reader.close();
+  				writer.close();
+  			}
         }
     }
 
-}
